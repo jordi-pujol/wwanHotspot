c079e9e46596e24f615334dd21c9691cbd32ccce
jordi-pujol <jordipujolp@gmail.com>
2017-09-30 09:19:28 +0200

    wwanHotspot.sh
    v1.0

--- files/wwanHotspot.sh
+++ files/wwanHotspot.sh
@@ -1,42 +1,58 @@
 #!/bin/sh
-#
-# Fix loss of AP when WWAN (Hotspot client) mode fails
-# by disabling the WWAN client.
-# Configuration contains a list of Hotspots to connect, will
-# re-enable the WWAN client when one becomes available.
-# Will look periodically for a Hotspot if ScanAuto is not null or 
-# else when a scan request is received.
-# Note: ScanAuto is not recommended because overloads the wifi interface,
-# is better request an scan to the daemon via ssh or telnet.
 
-# config variables, default values
-Debug=""
-ScanAuto=""
-Sleep=60
-SleepScanAuto="$((${Sleep}*10))"
+#  wwanHotspot
+#
+#  Wireless WAN Hotspot management application for OpenWrt routers.
+#  $Revision: 1.0 $
+#
+#  Copyright (C) 2017-2017 Jordi Pujol <jordipujolp AT gmail DOT com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#************************************************************************
+
+_log() {
+	logger -t "${NAME}" "${@}"
+	printf '%s %s\n' "$(date +'%Y-%m-%d %H:%M:%S')" \
+		"$(echo "${@}")" >&2
+}
 
 _sleep() {
 	local s="${1:-"${Slp}"}"
 	( set +x
+	printf '%s' "." >&2
 	while [ ${s} -gt 0 ]; do
 		sleep 1
-		printf '%s' "." >&2
 		s=$((${s}-1))
 	done ) &
 	PidSleep="${!}"
 	wait "${PidSleep}" || :
+	echo >&2
 	PidSleep=""
 }
 
 ScanRequested() {
 	[ -n "${PidSleep}" -a ${ScanRequest} -eq 0 ] || \
 		return 0
+	_log "Scan requested."
 	WwanErr=0
 	ScanRequest=${CfgSsidsCnt}
 	kill -TERM "${PidSleep}" || :
 }
 
 _exit() {
+	_log "Exit."
 	kill -TERM $(ps --no-headers --ppid "${PidDaemon}" -o pid) || :
 	wait || :
 }
@@ -44,15 +60,23 @@ _exit() {
 LoadConfig() {
 	local n=0 ssid
 
-	[ ! -s "/etc/config/${NAME}" ] || \
-	. "/etc/config/${NAME}"
+	_log "Loading configuration."
 
-	if [ -n "${Debug}" ]; then
-		set -x
-		exec > "/var/log/${NAME}" 2>&1
-	else
+	# config variables, default values
+	Debug=""
+	ScanAuto="y"
+	Sleep=60
+	SleepScanAuto="$((${Sleep}*10))"
+	net1_ssid=""
+
+	[ ! -s "/etc/config/${NAME}" ] || \
+		. "/etc/config/${NAME}"
+
+	rm -f "/var/log/${NAME}"
+	exec > "/var/log/${NAME}" 2>&1
+	[ -n "${Debug}" ] && \
+		set -x || \
 		set +x
-	fi
 
 	CfgSsids=""
 	while true; do
@@ -68,16 +92,38 @@ LoadConfig() {
 			CfgSsids="${CfgSsids}"$'\n'"${ssid}"
 		fi
 	done
+	WwanSsid="$(uci -q get wireless.@wifi-iface[1].ssid)" || :
 	[ -n "${CfgSsids}" ] || \
 		CfgSsids="${WwanSsid}"
-	[ -n "${CfgSsids}" ] || \
+	if [ -z "${CfgSsids}" ]; then
+		_log "Invalid configuration."
 		exit 1
+	fi
 	CfgSsidsCnt="$(printf '%s\n' "${CfgSsids}" | wc -l)"
-	WwanErrMax=$((${CfgSsidsCnt}*2))
+
+	ScanRequest=1
+	Slp=${Sleep}
+	WwanErr=0
+	Status=0
+}
+
+MustScan() {
+	[ ${ScanRequest} -eq 0 -a "${ScanAuto}" != "allways" ] || \
+		return 0
+	[ -n "${ScanAuto}" ] || \
+		return 1
+	local status
+	status="$(cat /sys/class/net/${IfaceWan}/operstate 2> /dev/null)" || \
+		return 0
+	[ -z "${status}" -o "${status}" = "down" ] || \
+		return 1
 }
 
 DoScan() {
 	local ssid scanned n i
+
+	MustScan || \
+		return 1
 
 	scanned="$(iw wlan0 scan | \
 		awk '$1 == "SSID:" {print}' | \
@@ -117,50 +163,45 @@ DoScan() {
 }
 
 WifiStatus() {
-	local CfgSsids CfgSsidsCnt ssid WwanSsid WwanErrMax
-
 	# internal variables, daemon scope
+	local CfgSsids CfgSsidsCnt ssid WwanSsid
+	local ScanRequest Slp WwanErr Status
 	local PidDaemon="${$}"
-	local ScanRequest=1
-	local Slp=${Sleep}
-	local WwanErr=0
-	local Status=0
 	local PidSleep=""
+	local IfaceWan="$(uci -q get network.wan.ifname)" || :
 
 	trap '_exit' EXIT
 
-	WwanSsid="$(uci -q get wireless.@wifi-iface[1].ssid)" || :
 	LoadConfig || exit 1
 
 	trap 'LoadConfig' HUP
 	trap 'ScanRequested' USR1
 
 	while :; do
+		_sleep
 		if iwinfo | grep -qsre "wlan0[[:blank:]]*ESSID: unknown"; then
 			uci set wireless.@wifi-iface[1].disabled=1
 			uci commit wireless
 			/etc/init.d/network restart
 			Slp=${Sleep}
 			if [ ${Status} != 1 ]; then
-				logger -t "${NAME}" \
-					"Disabling wireless device for Hotspot."
+				_log "Disabling wireless device for Hotspot."
 				Status=1
 				ScanRequest=1
 			fi
-			_sleep 5
+			continue
 		fi
+		WwanSsid="$(uci -q get wireless.@wifi-iface[1].ssid)" || :
 		if iwinfo | \
 		grep -qsre 'wlan0[[:blank:]]*ESSID: "'"${WwanSsid}"'"'; then
 			ScanRequest=0
 			WwanErr=0
 			Slp=${Sleep}
 			if [ ${Status} != 2 ]; then
-				logger -t "${NAME}" \
-					"Hotspot ${WwanSsid} is connected."
+				_log "Hotspot ${WwanSsid} is connected."
 				Status=2
 			fi
-		elif [ -n "${ScanAuto}" -o ${ScanRequest} -gt 0 ] && \
-		ssid="$(DoScan)"; then
+		elif ssid="$(DoScan)"; then
 			local n wifi_change=""
 			n="$(printf '%s\n' "${ssid}" | \
 				cut -f 1 -s -d ':')"
@@ -171,8 +212,7 @@ WifiStatus() {
 				eval key=\"\$net${n}_key\"
 				WwanErr=0
 				wifi_change=y
-				logger -t "${NAME}" \
-					"${ssid}" network found. Applying settings..
+				_log "${ssid} network found. Applying settings.."
 				uci set wireless.@wifi-iface[1].ssid="${ssid}"
 				uci set wireless.@wifi-iface[1].encryption="${encrypt}"
 				uci set wireless.@wifi-iface[1].key="${key}"
@@ -186,33 +226,29 @@ WifiStatus() {
 				uci commit wireless
 				wifi down
 				wifi up
-				logger -t "${NAME}" \
-					"Connecting to '${WwanSsid}'..."
+				_log "Connecting to '${WwanSsid}'..."
 				Slp=5
 			fi
 			WwanErr=$((${WwanErr}+1))
-			if [ ${WwanErr} -ge ${WwanErrMax} ] && \
+			if [ ${WwanErr} -ge ${CfgSsidsCnt} ] && \
 			[ ${Status} != 3 ]; then
 				ScanRequest=0
-				[ -z "${ScanAuto}" ] || \
+				[ "${ScanAuto}" = "allways" ] || \
 					Slp=${SleepScanAuto}
-				logger -t "${NAME}" \
-					"Error: can't connect to Hotspots, probably configuration is not correct."
+				_log "Error: can't connect to Hotspots, probably configuration is not correct."
 				Status=3
 			fi
 		else
 			WwanErr=0
 			Slp=${Sleep}
 			if [ ${Status} != 4 ]; then
-				logger -t "${NAME}" \
-					"A Hotspot is not available."
+				_log "A Hotspot is not available."
 				Status=4
 				Slp=5
 			fi
 		fi
 		[ ${ScanRequest} -eq 0 ] || \
 			ScanRequest=$((${ScanRequest}-1))
-		_sleep
 	done
 }
 
