45f55219b09b68240abbec49dd1757b1473f6834
jordi-pujol <jordipujolp@gmail.com>
2018-06-18 08:31:55 +0200

    build/make-package.sh v1.9
    build/make-package.sh v1.9

--- build/make-package.sh
+++ build/make-package.sh
@@ -3,7 +3,7 @@
 #  wwanHotspot
 #
 #  Wireless WAN Hotspot management application for OpenWrt routers.
-#  $Revision: 1.8 $
+#  $Revision: 1.9 $
 #
 #  Copyright (C) 2017-2018 Jordi Pujol <jordipujolp AT gmail DOT com>
 #
