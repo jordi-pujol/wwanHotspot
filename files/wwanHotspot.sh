b685161e8285aaa94c99a58fe8e166b688ddecb8
jordi-pujol <jordipujolp@gmail.com>
2018-08-16 10:47:27 +0200

    wwanHotspot 1.17
    wwanHotspot 1.17

--- files/wwanHotspot.sh
+++ files/wwanHotspot.sh
@@ -36,8 +36,7 @@ _sleep() {
 	[ -z "${Debug}" ] || \
 		_applog "sleeping ${Interval} seconds"
 	sleep ${Interval} > /dev/null 2>&1 &
-	PidSleep="${!}"
-	wait "${PidSleep}" || :
+	wait $((PidSleep=${!})) || :
 	[ -z "${Debug}" ] || \
 		_applog "sleeping ended"
 	PidSleep=""
@@ -514,7 +513,7 @@ WifiStatus() {
 			fi
 		fi
 		[ ${ScanRequest} -le 0 ] || \
-			ScanRequest=$((${ScanRequest}-1))
+			[ $((ScanRequest--)) ]
 	done
 }
 
